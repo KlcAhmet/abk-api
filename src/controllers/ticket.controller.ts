7f8fae8efdcab887321ee2c886b5e8b7745308b7
Ahmet Batuhan Kılıç <ahmetbatukilic@gmail.com>
2023-09-19 22:40:20 +0300

    Update when visitor send more ticket, detect ip and block request.

--- src/controllers/ticket.controller.ts
+++ src/controllers/ticket.controller.ts
@@ -2,7 +2,7 @@ import { inject } from '@loopback/core';
 import { post, Request, requestBody, response, ResponseObject, RestBindings } from '@loopback/rest';
 import { ITicket, IUserInfo } from '../models';
 import { CollectUsersInfo, CustomError } from '../mixins';
-import { createTicket } from '../repositories';
+import { createTicket, getTicket } from '../repositories';
 
 /**
  * OpenAPI response for ticket()
@@ -35,23 +35,49 @@ const TICKET_RESPONSE: ResponseObject = {
  * A simple controller to bounce back http requests
  */
 export class TicketController {
+  static createTicketIpCounter: string[] = [];
+
   constructor(@inject(RestBindings.Http.REQUEST) private req: Request) {}
 
   @post('/ticket')
   @response(200, TICKET_RESPONSE)
   async createTicket(@requestBody() ticket: ITicket): Promise<object> {
-    let statusCode: 200 | 400 | 422 = 200;
-    let errorMessage: string | undefined = undefined;
+    let statusCode: 200 | 400 | 422 | 429 = 200;
+    let tickets: Object[] = [];
 
     try {
       const userRemoteInfo: IUserInfo = new CollectUsersInfo(
         this.req,
       ).getUserRemoteInfo();
 
+      TicketController.createTicketIpCounter.push(
+        <string>userRemoteInfo.xForwardedFor || <string>userRemoteInfo.socket,
+      );
+      const sameIpCount = TicketController.createTicketIpCounter.filter(ip => {
+        return (
+          ip === userRemoteInfo.xForwardedFor || ip === userRemoteInfo.socket
+        );
+      }).length;
+      if (sameIpCount >= 100)
+        TicketController.createTicketIpCounter =
+          TicketController.createTicketIpCounter.slice(21);
+      if (sameIpCount < 3) {
+        const filter = userRemoteInfo.xForwardedFor
+          ? {
+              'userRemoteInfo.xForwardedFor': userRemoteInfo.xForwardedFor,
+            }
+          : {'userRemoteInfo.socket': userRemoteInfo.socket};
+        tickets = await getTicket(filter);
+      }
+
       if (
+        // validate tickets length for ddos attack protection (3 tickets per 1 minute)
+        sameIpCount < 3 &&
+        tickets.length < 2 &&
+        // validate ticket
         ticket.message &&
         ticket.message.length > 0 &&
-        ticket.message.length < 1000 &&
+        ticket.message.length <= 1000 &&
         ticket.name &&
         ticket.name.length > 0 &&
         ticket.name.length < 100 &&
@@ -64,15 +90,13 @@ export class TicketController {
           userRemoteInfo: userRemoteInfo,
         });
       } else {
-        errorMessage = 'Invalid ticket';
-        statusCode = 422;
-        new CustomError('createTicket-controller', errorMessage, statusCode);
+        statusCode = tickets.length >= 2 || sameIpCount >= 3 ? 429 : 422;
+        new CustomError('createTicket-controller', undefined, statusCode);
       }
 
       return {
         info: ticket,
         statusCode: statusCode,
-        errorMessage: errorMessage,
         date: new Date(),
         url: this.req.url,
         headers: Object.assign({}, this.req.headers),
@@ -80,7 +104,7 @@ export class TicketController {
     } catch (error) {
       new CustomError('createTicket-controller', error?.message, statusCode);
       return {
-        statusCode: 422,
+        statusCode: 500,
         date: new Date(),
         url: this.req.url,
         headers: Object.assign({}, this.req.headers),
--- src/repositories/ticket.ts
+++ src/repositories/ticket.ts
@@ -1,10 +1,20 @@
 import {ITicket, TicketModel} from '../models';
 import {CustomError} from '../mixins';
 
-export async function createTicket(ticket: ITicket): Promise<object> {
+export async function createTicket(ticket: ITicket): Promise<ITicket> {
   try {
     const newTicket = new TicketModel(ticket);
     return await newTicket.save();
+  } catch (error) {
+    new CustomError('createTicket-repository', error?.message);
+    throw new Error(error);
+  }
+}
+
+export async function getTicket(filter: {}): Promise<Object[]> {
+  try {
+    return await TicketModel.find(filter);
+    // return  await TicketModel.find({ name: 'john', age: { $gte: 18 } }).exec();
   } catch (error) {
     new CustomError('createTicket-repository', error?.message);
     throw new Error(error);
