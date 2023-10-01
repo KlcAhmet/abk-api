0840f01e21b1930354811c2370b4b7bb64e44c6d
Ahmet Batuhan Kılıç <ahmetbatukilic@gmail.com>
2023-10-01 03:40:53 +0300

    fix

--- src/models/ticket.ts
+++ src/models/ticket.ts
@@ -46,13 +46,11 @@ export const ticketSchema = new Schema<ITicket>(
     name: {
       type: String,
       required: true,
-      min: 1,
       max: 100,
     },
     mail: {
       type: String,
       required: true,
-      min: 5,
       max: 100,
       validate: {
         validator: (val: string) => {
@@ -64,7 +62,6 @@ export const ticketSchema = new Schema<ITicket>(
     message: {
       type: String,
       required: true,
-      min: 5,
       max: 1000,
     },
     userRemoteInfo: userInfoSchema,
